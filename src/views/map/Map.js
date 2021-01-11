1d69fae3da7e7e3fef981b0e6262fbe98a0b9640
david <david.weining@outlook.com>
2021-01-12 01:09:55 +0800

    地图加载

--- src/views/map/Map.js
+++ src/views/map/Map.js
@@ -6,8 +6,9 @@ class Map extends React.Component {
     componentDidMount() {
         // create map
         this.map = L.map('map', {
-            center: [49.8419, 24.0315],
-            zoom: 16,
+            center: [30.662205, 104.071887],
+            crs:L.CRS.EPSG3857,
+            zoom: 14,
             layers: [
                 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
