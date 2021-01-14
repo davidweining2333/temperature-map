1e02e7e6d30c1d2c20dd643df5a063d13b6e9f31
david <david.weining@outlook.com>
2021-01-15 00:18:54 +0800

    基础demo交互

--- src/views/map/Map.js
+++ src/views/map/Map.js
@@ -6,6 +6,7 @@ import 'antd/dist/antd.css';
 import { Table } from 'antd';
 import './Map.less';
 import 'leaflet/dist/leaflet.css';
+import * as echarts from "echarts";
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -54,6 +55,74 @@ class Map extends React.Component {
                 }
             ]
         }
+        this.temperatureCharts = void 0;
+        this.refreshCharts = function (record) {
+            let base = 20;
+            if (record) {
+                switch (record.key) {
+                    case "cd":
+                        base = 10;
+                        break;
+                    case "bj":
+                        base = 0;
+                        break;
+                    case "sh":
+                        base = 20;
+                        break;
+                }
+            }
+            let data = this.generateData(base);
+
+            this.temperatureCharts.setOption({
+                grid: { top: 10, bottom: 20, right: 20 },
+                title: {
+                    text: '气温趋势图'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                },
+                xAxis: {
+                    type: 'time',
+                    // data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                },
+                yAxis: {
+                    type: 'value',
+                    boundaryGap: [0, '100%'],
+                    splitLine: {
+                        show: false
+                    }
+                },
+                series: [{
+                    name: '气温（℃）',
+                    type: 'line',
+                    data,
+                    smooth: true
+                }]
+            });
+        };
+        this.randomData = function (now, oneDay, oneHour, value) {
+            now = new Date(+now + oneHour);
+            value = value + Math.random() * 10;
+            return {
+                name: now.toString(),
+                value: [
+                    [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/') + ` ${now.getHours()}:00:00`,
+                    Math.round(value)
+                ]
+            };
+        }
+
+        this.generateData = function (baseTemp) {
+            var data = [];
+            var now = +new Date();
+            var oneDay = 24 * 3600 * 1000;
+            var oneHour = 3600 * 1000;
+            var value = Math.random() * 1000;
+            for (var i = 0; i < 30 * 24; i++) {
+                data.push(this.randomData(now, oneDay, oneHour * i, baseTemp));
+            }
+            return data;
+        }
     }
     componentDidMount() {
         // create map
@@ -75,11 +144,17 @@ class Map extends React.Component {
         this.state.dataSource.forEach(item => {
             L.marker([item.lat, item.lang]).addTo(this.map)
         });
+        setTimeout(() => {
+            this.map.fitBounds(this.state.dataSource.map(item => [item.lat, item.lang]));
+        })
         // // add layer
         // this.layer = L.layerGroup().addTo(this.map);
         // this.map.setView(
         //     center, zoom
         // )
+        this.temperatureCharts = echarts.init(document.getElementById('tempuratureCharts'));
+        this.refreshCharts();
+
     }
     componentDidUpdate({ markerPosition }) {
         // check if position has changed 
@@ -92,7 +167,20 @@ class Map extends React.Component {
         return <div className="body">
             <div className="markes-table-area">
                 <h4>位置列表</h4>
-                <Table dataSource={this.state.dataSource} columns={this.state.columns} size="small" />
+                <Table dataSource={this.state.dataSource} columns={this.state.columns} size="small"
+                    onRow={record => {
+                        return {
+                            onClick: event => {
+                                this.map.setView([record.lat, record.lang]);
+                                this.refreshCharts(record);
+                                console.log(record)
+                            }, // 点击行
+                        };
+                    }} />
+            </div>
+            <div className="statistics-area">
+                {/* <h4>气温趋势图</h4> */}
+                <div id="tempuratureCharts"></div>
             </div>
             <div id="map"></div>
         </div>
